bbab2546ad363ccb7c37840319a04e82f374e5cb
marcelWCH <marcelw-ch@hotmail.com>
2022-08-09 15:06:55 +0200

    Projektdateien hinzufügen.

---
+++ BlazorAppSF/Program.cs
@@ -0,0 +1,58 @@
+using BlazorAppSF.Areas.Identity;
+using BlazorAppSF.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Syncfusion.Blazor;
+//vers. 20.2.0.36/38 -  18.4.0.48
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Njc3MDE4QDMyMzAyZTMyMmUzMFdHck9JTlZuZW1OT1FxZW1MRUxuY0E1RXRRb0t3MU9nK1JrZmxoVUQ4c1k9");
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSyncfusionBlazor();
+
+//builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = false; });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
